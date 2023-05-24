7e88a09e4a6827ba04956bd24e08ba9779ad2d77
Hüseyin <hkrabs@yandex.com>
2023-05-24 14:03:57 +0300

    Zoom toggle

--- src/components/PreviewImage.vue
+++ src/components/PreviewImage.vue
@@ -1,12 +1,18 @@
 <script setup>
+import { ref } from 'vue';
+
 defineProps({
   url: String,
   name: String,
 });
+
+const zoom = ref(false);
+
+const toggleZoom = () => zoom.value = !zoom.value;
 </script>
 
 <template>
-  <div class="container">
+  <div class="container" @click="toggleZoom" :class="{ 'zoom': zoom }">
     <img :src="url" :alt="name" class="image">
   </div>
 </template>
@@ -19,10 +25,22 @@ defineProps({
   display: flex;
   align-items: center;
   justify-content: center;
+  overflow: auto;
 }
 
 .image {
-  max-width:100%;
-  max-height:100%;
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.zoom {
+  text-align: unset;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+.zoom .image {
+  max-width: unset;
+  max-height: unset;
 }
 </style>
